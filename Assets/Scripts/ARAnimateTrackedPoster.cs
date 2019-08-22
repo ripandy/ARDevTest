6fcf47b8d96f46f7a4c99fa9c6ca4477563f6191
Ripandy Adha <ripandy.adha@gmail.com>
2019-08-22 16:42:22 +0900

    feat : basic AR Image Tracking functionality

---
+++ Assets/Scripts/ARAnimateTrackedPoster.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.XR.ARFoundation;
+using DG.Tweening;
+using UniRx;
+
+public class ARAnimateTrackedPoster : MonoBehaviour
+{
+    readonly string CHILD_PREFAB_NAME = "PosterTrackedPrefab";
+
+    public string assetBundlePath;
+
+    ARTrackedImageManager m_TrackedImageManager;
+
+    AssetBundle loadedBundle;
+    GameObject childPrefab;
+    Camera mainCamera;
+
+    bool animate = false;
+
+    void Awake()
+    {
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        StartCoroutine(LoadAssetBundle(assetBundlePath));
+        SetReactiveObservers();
+    }
+
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void SetReactiveObservers()
+    {
+        Observable.Interval(System.TimeSpan.FromMilliseconds(1000f))
+            .Subscribe(_ => {
+                Debug.Log("Camera position : " + mainCamera.transform.position);
+            });
+    }
+
+    IEnumerator LoadAssetBundle(string path)
+    {
+        using (var uwr = UnityWebRequestAssetBundle.GetAssetBundle(path))
+        {
+            Debug.Log("downloading asset bundle..");
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log("failed to download : " + uwr.error);
+            }
+            else
+            {
+                Debug.Log("asset bundle downloaded.. loading asset bundle..");
+                loadedBundle = DownloadHandlerAssetBundle.GetContent(uwr);
+            }
+        }
+
+        yield return null;
+
+        if (loadedBundle != null)
+        {
+            Debug.Log("Asset Bundle loaded successfully");
+            childPrefab = loadedBundle.LoadAsset(CHILD_PREFAB_NAME) as GameObject;
+        }
+        else
+        {
+            Debug.Log("failed to load Asset Bundle : " + path);
+        }
+    }
+    
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+            AnimateChildAppear();
+
+        foreach (var trackedImage in eventArgs.updated)
+            AnimateChildAppear();
+    }
+
+    void AnimateChildAppear()
+    {
+        if (!animate)
+        {
+            if (childPrefab != null)
+            {
+                Debug.Log("starting animation");
+
+                animate = true;
+
+                var child = Instantiate(childPrefab);
+
+                child.transform.DOLocalMoveY(1f, 1f);
+                child.transform.DOScale(Vector3.one, 1f)
+                    .SetDelay(0.5f);
+                    // .OnComplete(() => {
+                    //     AnimateChildApproach(child.transform);
+                    // });
+            }
+            else
+            {
+                Debug.Log("no child prefab");
+            }
+        }
+    }
+
+    void AnimateChildApproach(Transform child)
+    {
+        Debug.Log("animate approach..");
+        child.SetParent(mainCamera.transform, true);
+        child.DOLocalMove(new Vector3(0f, 0f, 0.05f), 0.3f).SetDelay(1f);
+    }
+}
---
+++ Assets/Scripts/ARPlacementController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementController : MonoBehaviour
+{
+    public GameObject charaPrefab;
+    public GameObject targetIndicator;
+
+    GameObject chara = null;
+    ARRaycastManager raycastManager;
+    Pose placementPose;
+    bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        Debug.Log("raycastManager? " + raycastManager);
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        
+        if (placementPoseIsValid && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                UpdateObject();
+            }
+        }   
+    }
+
+    private void UpdateObject()
+    {
+        if (chara == null)
+            chara = Instantiate(charaPrefab, placementPose.position, placementPose.rotation);
+        else
+            chara.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        targetIndicator.SetActive(placementPoseIsValid);
+        if (placementPoseIsValid)
+        {
+            targetIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0f, cameraForward.z).normalized * -1f;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
